7874d2b3deeb52b6d87ee4a08bf09e3ab890c615
ab00zar <rajabi.aboozar@gmail.com>
2025-04-07 14:30:22 +0200

    configs updated

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  # Code is not reloaded between requests.
+  # Code is not reloaded between controllers.
   config.enable_reloading = false
 
   # Eager load code on boot for better performance and memory savings (ignored by Rake tasks).
@@ -81,8 +81,8 @@ Rails.application.configure do
 
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
-  #   "example.com",     # Allow requests from example.com
-  #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
+  #   "example.com",     # Allow controllers from example.com
+  #   /.*\.example\.com/ # Allow controllers from subdomains like `www.example.com`
   # ]
   #
   # Skip DNS rebinding protection for the default health check endpoint.
